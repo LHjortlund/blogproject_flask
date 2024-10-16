b80c89c8a59c0da28ae65805fd710449f17a6702
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-16 14:03:58 +0200

    finsh challenge

--- post.py
+++ post.py
@@ -1,2 +1,11 @@
+import requests
+
+BLOG_URL = "https://api.npoint.io/c10781c59f247ee64d13"
+
 class Post:
-    pass
+    def get_post(self, index=None):
+        response = requests.get(BLOG_URL)
+        all_posts = response.json()
+        if index is not None:
+            return [all_posts[index]]
+        return all_posts
